58200ae36e4749f5db74fd8c9a8c24523366b673
RossieK <rosi.kirova96@gmail.com>
2020-12-09 20:20:54 +0200

    Add images + wizard to the game area

--- src/script.js
+++ src/script.js
@@ -7,5 +7,9 @@ gameStart.addEventListener('click', onGameStart);
 
 function onGameStart() {
     gameStart.classList.add('hide');
-    alert("Game started!");
+    const wizard = document.createElement("div");
+    wizard.classList.add('wizard');
+    wizard.style.top = '200px';
+    wizard.style.left = '300px';
+    gameArea.appendChild(wizard);
 }
