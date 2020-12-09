5143ad75a7746972de447757a3941529b9ba6da2
RossieK <rosi.kirova96@gmail.com>
2020-12-09 20:28:19 +0200

    Add user key registration

--- src/script.js
+++ src/script.js
@@ -4,6 +4,10 @@ const gameOver = document.querySelector('.game-over');
 const gameScore = document.querySelector('.game-score');
 
 gameStart.addEventListener('click', onGameStart);
+document.addEventListener('keydown', onKeyDown);
+document.addEventListener('keyup', onKeyUp);
+
+let keys = {};
 
 function onGameStart() {
     gameStart.classList.add('hide');
@@ -12,4 +16,14 @@ function onGameStart() {
     wizard.style.top = '200px';
     wizard.style.left = '300px';
     gameArea.appendChild(wizard);
+}
+
+function onKeyDown(e) {
+    e.preventDefault();
+    keys[e.code] = true;
+    console.log(keys);
+}
+
+function onKeyUp(e) {
+    keys[e.code] = false;
 }
