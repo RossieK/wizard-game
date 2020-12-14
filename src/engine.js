94b3f07fb3a3a5ea1559f11863033f7b8f11acd2
RossieK <rosi.kirova96@gmail.com>
2020-12-14 19:33:14 +0200

    Reverting to previous version

--- src/engine.js
+++ src/engine.js
@@ -1,48 +1,20 @@
 let keys = {};
 
-const initialState = ({ areaWidth }) => ({
-    player: {
-        x: 150,
-        y: 150,
-        width: 0,
-        height: 0,
-        lastTimeFiredBall: 0
-    },
-    scene: {
-        score: 0,
-        lastCloudSpawn: 0,
-        lastBugSpawn: 0,
-        isActiveGame: true,
-        areaWidth,
-        attackWidth: 40,
-        attackHeight: 40
-    },
-    clouds: [],
-    attacks: [],
-    bugs: []
-});
+let player = {
+    x: 150,
+    y: 150,
+    width: 0,
+    height: 0,
+    lastTimeFiredBall: 0
+};
 
-const nextClouds = (state) => (state.clouds);
-const nextPlayer = (state) => (state.player);
-const nextScene = (state) => (state.scene);
-const nextAttacks = (state) => (state.attacks
-    .filter(a => {
-        if (a.x + state.scene.attackWidth > state.scene.areaWidth) {
-            (a.el).parentElement.removeChild(a.el);
-            return false;
-        }
-        return true;
-    })
-    .map(a => ({...a, x: a.x + game.speed * game.fireballMultiplier })));
-const nextBugs = (state) => (state.bugs);
+let scene = {
+    score: 0,
+    lastCloudSpawn: 0,
+    lastBugSpawn: 0,
+    isActiveGame: true
+}
 
-const next = (state) => ({
-    player: nextPlayer(state),
-    scene: nextScene(state),
-    clouds: nextClouds(state),
-    attacks: nextAttacks(state),
-    bugs: nextBugs(state)
-});
 
 function isCollision(firstEl, secondEl) {
     let firstRect = firstEl.getBoundingClientRect();
@@ -52,23 +24,17 @@ function isCollision(firstEl, secondEl) {
 }
 
 function gameOverAction() {
-    state.scene.isActiveGame = false;
+    scene.isActiveGame = false;
     gameOver.classList.remove('hide');
 }
 
-function addFireball(state) {
+function addFireball(player) {
     let fireball = document.createElement('div');
 
     fireball.classList.add('fireball');
-    fireball.style.top = (state.player.y + state.player.height / 3 - 5) + 'px';
-    fireball.x = state.player.x + state.player.width;
+    fireball.style.top = (player.y + player.height / 3 - 5) + 'px';
+    fireball.x = player.x + player.width;
     fireball.style.left = fireball.x + 'px';
-
-    state.attacks.push({
-        x: state.player.x,
-        y: state.player.y + state.player.height / 3 - 5,
-        el: fireball
-    });
 
     gameArea.appendChild(fireball);
 }
--- src/renderer.js
+++ src/renderer.js
@@ -8,45 +8,40 @@ gameStart.addEventListener('click', onGameStart);
 document.addEventListener('keydown', onKeyDown);
 document.addEventListener('keyup', onKeyUp);
 
-let state = initialState({
-    areaWidth: gameArea.offsetWidth
-});
-
 function onGameStart() {
     gameStart.classList.add('hide');
     const wizard = document.createElement("div");
     wizard.classList.add('wizard');
-    wizard.style.top = state.player.y + 'px';
-    wizard.style.left = state.player.x + 'px';
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
     gameArea.appendChild(wizard);
-    state.player.width = wizard.offsetWidth;
-    state.player.height = wizard.offsetHeight;
+    player.width = wizard.offsetWidth;
+    player.height = wizard.offsetHeight;
     window.requestAnimationFrame(frame(0));
 }
 
 const frame = t1 => t2 => {
     if (t2 - t1 > game.frameLength) {
-        state = next(state);
-        gameAction(t2, state);
-        state.scene.isActiveGame && window.requestAnimationFrame(frame(t2));
+        gameAction(t2);
+        scene.isActiveGame && window.requestAnimationFrame(frame(t2));
     } else {
         window.requestAnimationFrame(frame(t1));
     }
 }
 
-function gameAction(timestamp, state) {
+function gameAction(timestamp) {
     const wizard = document.querySelector('.wizard');
 
     //apply gravitation
-    let isInAir = (state.player.y + state.player.height) <= gameArea.offsetHeight;
+    let isInAir = (player.y + player.height) <= gameArea.offsetHeight;
     if (isInAir) {
-        state.player.y += game.speed;
+        player.y += game.speed;
     }
 
-    state.scene.score++;
+    scene.score++;
 
     //add bugs
-    if (timestamp - state.scene.lastBugSpawn > game.bugSpawnInterval + 5000 * Math.random()) {
+    if (timestamp - scene.lastBugSpawn > game.bugSpawnInterval + 5000 * Math.random()) {
         let bug = document.createElement('div');
         bug.classList.add('bug');
         bug.x = gameArea.offsetWidth - 60;
@@ -54,11 +49,11 @@ function gameAction(timestamp, state) {
         bug.style.top = (gameArea.offsetHeight - 60) * Math.random() + 'px';
 
         gameArea.appendChild(bug);
-        state.scene.lastBugSpawn = timestamp;
+        scene.lastBugSpawn = timestamp;
     }
 
     //add clouds
-    if (timestamp - state.scene.lastCloudSpawn > game.cloudSpawnInterval + 20000 * Math.random()) {
+    if (timestamp - scene.lastCloudSpawn > game.cloudSpawnInterval + 20000 * Math.random()) {
         let cloud = document.createElement('div');
         cloud.classList.add('cloud');
         cloud.x = gameArea.offsetWidth - 200;
@@ -66,7 +61,7 @@ function gameAction(timestamp, state) {
         cloud.style.top = (gameArea.offsetHeight - 200) * Math.random() + 'px';
 
         gameArea.appendChild(cloud);
-        state.scene.lastCloudSpawn = timestamp;
+        scene.lastCloudSpawn = timestamp;
     }
 
     //modify bugs position
@@ -93,33 +88,38 @@ function gameAction(timestamp, state) {
 
     //modify fireballs position
     let fireballs = document.querySelectorAll('.fireball');
-    state.attacks.forEach(a => {
-        a.el.style.left = a.x + 'px';
+    fireballs.forEach(fireball => {
+        fireball.x += game.speed * game.fireballMultiplier;
+        fireball.style.left = fireball.x + 'px';
+
+        if (fireball.x + fireball.offsetWidth > gameArea.offsetWidth) {
+            fireball.parentElement.removeChild(fireball);
+        }
     });
 
     // register user input 
-    if (keys.ArrowUp && state.player.y > 0) {
-        state.player.y -= game.speed * game.movingMultiplier;
+    if (keys.ArrowUp && player.y > 0) {
+        player.y -= game.speed * game.movingMultiplier;
     }
 
     if (keys.ArrowDown && isInAir) {
-        state.player.y += game.speed * game.movingMultiplier;
+        player.y += game.speed * game.movingMultiplier;
     }
 
-    if (keys.ArrowLeft && state.player.x > 0) {
-        state.player.x -= game.speed * game.movingMultiplier;
+    if (keys.ArrowLeft && player.x > 0) {
+        player.x -= game.speed * game.movingMultiplier;
     }
 
-    if (keys.ArrowRight && state.player.x + state.player.width < gameArea.offsetWidth) {
-        state.player.x += game.speed * game.movingMultiplier;
+    if (keys.ArrowRight && player.x + player.width < gameArea.offsetWidth) {
+        player.x += game.speed * game.movingMultiplier;
     }
 
-    if (keys.Space && timestamp - state.player.lastTimeFiredBall > game.fireInterval) {
+    if (keys.Space && timestamp - player.lastTimeFiredBall > game.fireInterval) {
         wizard.classList.add('wizard-fire');
 
         // add fireball
-        addFireball(state);
-        state.player.lastTimeFiredBall = timestamp;
+        addFireball(player);
+        player.lastTimeFiredBall = timestamp;
 
     } else {
         wizard.classList.remove('wizard-fire');
@@ -133,18 +133,18 @@ function gameAction(timestamp, state) {
 
         fireballs.forEach(fireball => {
             if (isCollision(fireball, bug)) {
-                state.score += game.bugKillBonus;
+                scene.score += game.bugKillBonus;
                 bug.parentElement.removeChild(bug);
-                //fireball.parentElement.removeChild(fireball);
+                fireball.parentElement.removeChild(fireball);
             }
         })
     });
 
     //apply movement
-    wizard.style.top = state.player.y + 'px';
-    wizard.style.left = state.player.x + 'px';
+    wizard.style.top = player.y + 'px';
+    wizard.style.left = player.x + 'px';
 
 
     //apply score
-    gamePoints.textContent = state.scene.score;
+    gamePoints.textContent = scene.score;
 }
